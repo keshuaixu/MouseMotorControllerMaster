50db90a7defc93e0b67e8dcbe08dc5179d72c65a
Keshuai Xu <i@xks.im>
2015-04-06 22:15:27 -0400

    working.

--- MotorControllerMaster.cpp
+++ MotorControllerMaster.cpp
@@ -7,8 +7,26 @@ void MotorControllerMaster::setAcceleration(unsigned int forwardAcceleration, un
 	byte send[9];
 	send[0] = COMMAND_SETACCELERATION;
 	send[1] = forwardAcceleration;
-	send[1] = forwardAcceleration >> 8;
+	send[2] = forwardAcceleration >> 8;
+	send[3] = ccwAcceleration;
+	send[4] = ccwAcceleration >> 8;
+	send[5] = forwardDeceleration;
+	send[6] = forwardDeceleration >> 8;
+	send[7] = ccwDeceleration;
+	send[8] = ccwDeceleration >> 8;
 	Wire.beginTransmission(MOTOR_CONTROLLER_ADDRESS);
 	Wire.write(send,9);
+	Wire.endTransmission();
+}
+
+void MotorControllerMaster::goVelocity(int forwardVelocity, int ccwVelocity){
+	byte send[5];
+	send[0] = COMMAND_GOVELOCITY;
+	send[1] = forwardVelocity;
+	send[2] = forwardVelocity >> 8;
+	send[3] = ccwVelocity;
+	send[4] = ccwVelocity >> 8;
+	Wire.beginTransmission(MOTOR_CONTROLLER_ADDRESS);
+	Wire.write(send,5);
 	Wire.endTransmission();
 }
